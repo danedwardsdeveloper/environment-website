46885c3b3ab5fba483ac56161dda94c12d41c5bb
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-29 10:49:07 +0000

    Create MenuBar.tsx

---
+++ src/app/MenuBar.tsx
@@ -0,0 +1,16 @@
+export default function MenuBar() {
+  return (
+    <ul>
+      <li>{`Home`}</li>
+      <li>{`About Us`}</li>
+      <li>{`Services`}</li>
+      <li>{`Current Grant Programmes`}</li>
+      <li>{`Past Programmes`}</li>
+      <li>{`Blogs`}</li>
+      <li>{`Toolkit`}</li>
+      <li>{`Events`}</li>
+      <li>{`Case Studies`}</li>
+      <li>{`Contact`}</li>
+    </ul>
+  )
+}
